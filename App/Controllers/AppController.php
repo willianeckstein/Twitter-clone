61660bba1c78fd1b077e522076c70853eca8da7c
Willian Eckstein <48301841+willianeckstein@users.noreply.github.com>
2021-04-03 16:27:50 -0300

    Autenticar usuários
    Inserido também criptografia md5 na senha

---
+++ App/Controllers/AppController.php
@@ -0,0 +1,17 @@
+<?php
+	namespace App\Controllers;
+	use MF\Controller\Action;
+	use MF\Model\Container;
+
+	class AppController extends Action {
+		public function timeline() {
+			session_start();
+
+			if($_SESSION['id'] != '' && $_SESSION['nome'] != '') {
+				$this->render('timeline');
+			} else {
+				header('location: /');
+			}
+		}
+	}
+?>
--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -5,16 +5,16 @@
 
 	class IndexController extends Action {
 		public function index() {
-
+			$this->view->login = isset($_GET['login']) ? $_GET['login'] : '';
 			$this->render('index');
 		}
 
 		public function inscreverse() {
 			$this->view->usuario = array(
-					'nome' => '';
-					'email' => '';
-					'senha' => '';
-				)
+					'nome' => '',
+					'email' => '',
+					'senha' => ''
+				);
 
 			$this->view->erroCadastro = false;
 
@@ -25,7 +25,7 @@
 			$usuario = Container::getModel('Usuario');
 			$usuario->__set('nome', $_POST['nome']);
 			$usuario->__set('email', $_POST['email']);
-			$usuario->__set('senha', $_POST['senha']);	
+			$usuario->__set('senha', md5($_POST['senha']));	
 
 			if($usuario->validarCadastro() && count($usuario->getUsuarioPorEmail()) == 0) {
 				$usuario->salvar();	
@@ -33,10 +33,10 @@
 				$this->render('cadastro');			
 			} else {
 				$this->view->usuario = array(
-					'nome' => $_POST['nome'];
-					'email' => $_POST['email'];
-					'senha' => $_POST['senha'];
-				)
+					'nome' => $_POST['nome'],
+					'email' => $_POST['email'],
+					'senha' => $_POST['senha']
+				);
 
 				$this->view->erroCadastro = true;
 
